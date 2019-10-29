a6d64cfa47d874f70fc7cab48f2fba3ff3d1a700
unknown <m.ndlovu@alustudent.com>
2019-10-29 08:19:52 +0200

    Added navigation using Angular Router

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Album } from '../album';
 import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-tracklisting',
